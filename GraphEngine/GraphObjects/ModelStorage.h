a6780bbf3828be430e62a3290954ba7f5fbcae4a
Grigoriy Pisarenko <ga_pisarenko@mail.ru>
2023-03-09 10:40:40 +0300

    Rewrited ModelStorage

--- GraphEngine/GraphObjects/ModelStorage.h
+++ GraphEngine/GraphObjects/ModelStorage.h
@@ -35,7 +35,9 @@ namespace gre {
 			glBindBuffer(GL_COPY_WRITE_BUFFER, 0);
 			glBindBuffer(GL_COPY_READ_BUFFER, 0);
 
+#ifdef _DEBUG
 			check_gl_errors(__FILE__, __LINE__, __func__);
+#endif // _DEBUG
 		}
 
 		ModelStorage(ModelStorage&& other) noexcept {
@@ -64,7 +66,9 @@ namespace gre {
 
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+#ifdef _DEBUG
 			check_gl_errors(__FILE__, __LINE__, __func__);
+#endif // _DEBUG
 			return matrix_buffer_;
 		}
 
@@ -73,12 +77,16 @@ namespace gre {
 			glBufferSubData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 16 * memory_id, sizeof(GLfloat) * 16, &std::vector<GLfloat>(models_[memory_id].second)[0]);
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+#ifdef _DEBUG
 			check_gl_errors(__FILE__, __LINE__, __func__);
+#endif // _DEBUG
 		}
 
 		void deallocate() {
 			glDeleteBuffers(1, &matrix_buffer_);
+#ifdef _DEBUG
 			check_gl_errors(__FILE__, __LINE__, __func__);
+#endif // _DEBUG
 
 			matrix_buffer_ = 0;
 		}
@@ -86,57 +94,61 @@ namespace gre {
 		void swap(ModelStorage& other) noexcept {
 			std::swap(matrix_buffer_, other.matrix_buffer_);
 			std::swap(max_count_models_, other.max_count_models_);
-			std::swap(models_index_, other.models_index_);
-			std::swap(free_model_id_, other.free_model_id_);
-			std::swap(models_, other.models_);
+			models_index_.swap(other.models_index_);
+			free_model_id_.swap(other.free_model_id_);
+			models_.swap(other.models_);
 		}
 
 	public:
-		using Iterator = std::vector<std::pair<size_t, Matrix4x4>>::const_iterator;
+		using ConstIterator = std::vector<std::pair<size_t, Matrix4x4>>::const_iterator;
 
 		const Matrix4x4& operator[](size_t id) const {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "operator[], invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			return models_[models_index_[id]].second;
 		}
 
-		ModelStorage& set(size_t id, const Matrix4x4& matrix) {
+		void set(size_t id, const Matrix4x4& matrix) {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "set, invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			models_[models_index_[id]].second = matrix;
-
-			glBindBuffer(GL_ARRAY_BUFFER, matrix_buffer_);
-			glBufferSubData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 16 * models_index_[id], sizeof(GLfloat) * 16, &std::vector<GLfloat>(matrix)[0]);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-			check_gl_errors(__FILE__, __LINE__, __func__);
-			return *this;
+			update_matrix(models_index_[id]);
 		}
 
 		Matrix4x4 get(size_t id) const {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "get, invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			return models_[models_index_[id]].second;
 		}
 
 		size_t get_memory_id(size_t id) const {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "get_memory_id, invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			return models_index_[id];
 		}
 
 		size_t get_id(size_t memory_id) const {
+#ifdef _DEBUG
 			if (models_.size() <= memory_id) {
 				throw GreOutOfRange(__FILE__, __LINE__, "get_id, invalid memory id.\n\n");
 			}
+#endif // _DEBUG
 
 			return models_[memory_id].first;
 		}
@@ -161,18 +173,20 @@ namespace gre {
 			return models_.empty();
 		}
 
-		Iterator begin() const noexcept {
+		ConstIterator begin() const noexcept {
 			return models_.begin();
 		}
 
-		Iterator end() const noexcept {
+		ConstIterator end() const noexcept {
 			return models_.end();
 		}
 
-		ModelStorage& erase(size_t id) {
+		void erase(size_t id) {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "erase, invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			free_model_id_.push_back(id);
 
@@ -183,25 +197,26 @@ namespace gre {
 
 			models_.pop_back();
 			models_index_[id] = std::numeric_limits<size_t>::max();
-			return *this;
 		}
 
-		ModelStorage& clear() noexcept {
+		void clear() noexcept {
 			models_index_.clear();
 			free_model_id_.clear();
 			models_.clear();
-			return *this;
 		}
 
 		size_t insert(const Matrix4x4& matrix) {
+#ifdef _DEBUG
 			if (models_.size() == max_count_models_) {
 				throw GreRuntimeError(__FILE__, __LINE__, "insert, too many instances created.\n\n");
 			}
+#endif // _DEBUG
 
 			size_t free_model_id = models_index_.size();
 			if (free_model_id_.empty()) {
 				models_index_.push_back(models_.size());
-			} else {
+			}
+			else {
 				free_model_id = free_model_id_.back();
 				free_model_id_.pop_back();
 				models_index_[free_model_id] = models_.size();
@@ -212,24 +227,26 @@ namespace gre {
 			return free_model_id;
 		}
 
-		ModelStorage& change_left(size_t id, const Matrix4x4& matrix) {
+		void change_left(size_t id, const Matrix4x4& matrix) {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "change_left, invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			models_[models_index_[id]].second = matrix * models_[models_index_[id]].second;
 			update_matrix(models_index_[id]);
-			return *this;
 		}
 
-		ModelStorage& change_right(size_t id, const Matrix4x4& matrix) {
+		void change_right(size_t id, const Matrix4x4& matrix) {
+#ifdef _DEBUG
 			if (!contains(id)) {
 				throw GreOutOfRange(__FILE__, __LINE__, "change_left, invalid model id.\n\n");
 			}
+#endif // _DEBUG
 
 			models_[models_index_[id]].second *= matrix;
 			update_matrix(models_index_[id]);
-			return *this;
 		}
 
 		~ModelStorage() {
